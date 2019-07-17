41a2e1efef1943b5cff23bd88a6440c3e43a6182
Vineel Pratap <vineelkpratap@fb.com>
2019-07-17 09:10:47 -0700

    Update arch_stats script to include TDS, output #params
    Summary: As per title
    
    Reviewed By: awni
    
    Differential Revision: D16296428
    
    fbshipit-source-id: 50677da01b223dfe2c4637d09f80ec85f0bbc12f

--- scripts/arch_stats.py
+++ scripts/arch_stats.py
@@ -19,6 +19,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "-a", "--arch", type=str, required=True, help="Path to W2L architecture file"
     )
+    parser.add_argument(
+        "-nin",
+        "--num_input_feat",
+        type=int,
+        default=80,
+        help="number of input features",
+    )
+    parser.add_argument(
+        "-nout",
+        "--num_output_feat",
+        type=int,
+        default=30,
+        help="number of target classes",
+    )
     args = parser.parse_args()
 
     if not os.path.exists(args.arch):
@@ -27,9 +41,12 @@ if __name__ == "__main__":
 
     overall_kw = 1
     overall_dw = 1
+    params = 0
 
     with open(args.arch, "r") as arch:
         for line in arch:
+            line = line.replace("NLABEL", str(args.num_output_feat))
+            line = line.replace("NFEAT", str(args.num_input_feat))
             line = line.strip()
             if not line or line.startswith("#"):
                 continue
@@ -41,16 +58,28 @@ if __name__ == "__main__":
             kw = 1
             dw = 1
             dil = 1
-            if layer == "C" or layer == "C1":
+            if layer == "L":
+                inC = int(linesplit[1])
+                outC = int(linesplit[2])
+                params += inC * outC * kw
+            elif layer == "C" or layer == "C1":
+                inC = int(linesplit[1])
+                outC = int(linesplit[2])
                 kw = int(linesplit[3])
                 dw = int(linesplit[4])
                 if len(linesplit) > 6:
                     dil = int(linesplit[6])
+                params += inC * outC * kw
             elif layer == "C2":
+                inC = int(linesplit[1])
+                outC = int(linesplit[2])
                 kw = int(linesplit[3])
+                kwy = int(linesplit[4])
+                outC = int(linesplit[1])
                 dw = int(linesplit[5])
                 if len(linesplit) > 9:
                     dil = int(linesplit[9])
+                params += inC * outC * kw * kwy
             elif layer == "CL":
                 kw = int(linesplit[2])
                 # strided/dilated ConvLinear layers only for now
@@ -58,10 +87,16 @@ if __name__ == "__main__":
                 kw = int(linesplit[1])
                 dw = int(linesplit[3])
                 # dilated pooling not supported
+            elif layer == "TDS":
+                c = int(linesplit[1])
+                kw = int(linesplit[2])
+                h = int(linesplit[3])
+                params += c * c * (kw + 2 * h * h)
 
             overall_kw += (kw - 1) * overall_dw * dil
             overall_dw *= dw
 
 print("---- Arch Stats ----")
-print("Receptive Field (kw) :", overall_kw)
-print("Overall Stride (dw)  :", overall_dw)
+print("Receptive Field (kw)   :", overall_kw)
+print("Overall Stride (dw)    :", overall_dw)
+print("Overall Params (approx):", params)
