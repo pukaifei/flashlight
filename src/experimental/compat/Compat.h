f4d07bd0cb921ac96917665ea12a02746d4b9107
Ronan Collobert <locronan@fb.com>
2020-01-11 15:31:13 -0800

    Introduce TransformerCriterion (s2s) and add training support.
    Summary:
    Introduce TransformerCriterion (s2s) and add training support.
    
    Brought to you by Edouard Grave (EdouardGrave EdouardGrave) and Gabriel Synnaeve (syhw syhw).
    
    Reviewed By: syhw, jacobkahn
    
    Differential Revision: D19205487
    
    fbshipit-source-id: cca40a7cb1697703ed7996428cbd5c6c871c42c8

--- src/experimental/compat/Compat.h
+++ src/experimental/compat/Compat.h
@@ -1,1 +1,11 @@
 #include "experimental/compat/TransformerLayer.h"
+
+namespace w2l {
+
+DECLARE_int64(decoder_layers);
+DECLARE_double(decoder_dropout);
+DECLARE_double(decoder_layerdrop);
+
+void initCompat();
+
+} // namespace w2l
